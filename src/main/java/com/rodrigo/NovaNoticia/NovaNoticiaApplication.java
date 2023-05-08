c56cb4d9a3f6017c2761151756bd12cce5725451
Rodrigopca42 <rodrigopca426281@gmail.com>
2023-05-08 17:11:42 -0300

    Projeto criado

---
+++ src/main/java/com/rodrigo/NovaNoticia/NovaNoticiaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.NovaNoticia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaNoticiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaNoticiaApplication.class, args);
+	}
+
+}
